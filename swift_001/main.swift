9e418904b5fe5638b5a9f787959d83a7020bce15
Chum Ratha <chumratha@chums-macbook-pro.local>
2015-12-09 23:25:29 +0700

    Initial Commit

---
+++ swift_001/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift_001
+//
+//  Created by Chum Ratha on 12/9/15.
+//  Copyright © 2015 Chum Ratha. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
