a93aaaca6a55f17c1dea9bdd738d02da85422bc1
chum ratha <chumratha.corel@gmail.com>
2016-01-18 00:11:05 +0700

    change some statement

--- swift_001/main.swift
+++ swift_001/main.swift
@@ -8,5 +8,86 @@
 
 import Foundation
 
-print("Hello, World!")
+/*var i=4;
+var j=9;
+i=20;
+var total="Total is equeal ";
+NSLog("%@  %d", total,(i+j));
+let x=6;
+
+print(x);*/
+
+
+/*var i:Int=4
+print(i)
+
+let label="I love Cambodia"
+let result:Double=7
+let str=label+String(result)
+print(str)*/
+/*var str="game"
+str="hello"
+var i=10
+let c="I lvoe \(i)"
+print(c)*/
+/*typealias _int=Int
+var i:_int=5;
+print(i)
+
+var x="game"
+print(x)
+
+var c:Float=6
+print(c)*/
+/*var តាម="I love Cambodia"
+var i:Int
+i=7
+print(i)
+print(តាម)*/
+
+/*var i:Int?
+var str:String?="i lvove you"
+var item=String(i)+str!
+
+print(item)*/
+
+/*var str:String?
+
+print("I love Cambodia \(str)"+"Gaemfa"+str!)*/
+var str:String?="I love Cambodia"
+if let game=str
+{
+    print("have value is :\(game)")
+}
+else
+{
+    print("No value")
+}
+
+var i:Int?=0
+i=100
+if let x=i
+{
+    print("Zero")
+}
+var c=10
+switch (c)
+{
+case 1:
+    print("one")
+    break
+default:
+    print("hello")
+}
+if c==10
+{
+
+}
+print("hello world")
+
+
+
+
+
+
 
